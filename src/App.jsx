1937894991162df0f16697d3896b7fa014077d94
TJSXY <138418058+TJSXY@users.noreply.github.com>
2024-08-23 15:09:31 +0700

    first

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import { Button } from '@mui/material';
+
+function App() {
+  return (
+    <Button variant="contained">Contained</Button>
+  );
+}
+
+export default App;
